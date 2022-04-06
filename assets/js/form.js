521b1077c67347bd56a8ab84782b0b1c77b15a74
John Netzel <johnrnetzel@gmail.com>
2022-04-06 11:38:43 -0800

    add submit listener to form

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,1 +1,11 @@
-alert();
+const projectNameEl = $("#project-name");
+const projectTypeEl = $("#project-type");
+const hourlyWageEl = $("#project-wage");
+const dueDateEl = $("#project-due-date");
+const submitButtonEl = $("#project-form");
+
+submitButtonEl.on("submit", function(event)
+{
+    event.preventDefault(); // Default behavior is to reload page
+    alert(projectNameEl.val());
+});
