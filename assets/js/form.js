c8c9a84987616ec4c9708a20e35f1165a622713b
John Netzel <johnrnetzel@gmail.com>
2022-04-06 11:44:58 -0800

    add function to pack project form submission into object

--- assets/js/form.js
+++ assets/js/form.js
@@ -7,5 +7,19 @@ const submitButtonEl = $("#project-form");
 submitButtonEl.on("submit", function(event)
 {
     event.preventDefault(); // Default behavior is to reload page
-    alert(projectNameEl.val());
+    alert(collectProjectFormData());
 });
+
+/**
+ * Collects input from the project form and saves it to an object
+ * @returns an object with the form data as its properties
+ */
+function collectProjectFormData()
+{
+    var projectData = {};
+    projectData.name = projectNameEl.val();
+    projectData.type = projectTypeEl.val();
+    projectData.wage = hourlyWageEl.val();
+    projectData.dueDate = dueDateEl.val();
+    return projectData;
+}
